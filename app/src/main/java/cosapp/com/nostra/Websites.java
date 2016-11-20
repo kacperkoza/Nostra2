8aa7d50728401646c4a6965368822e86df4bc95b
Kacper Koza <kkoza11@gmail.com>
2016-11-20 15:42:37 +0100

    update

--- app/src/main/java/cosapp/com/nostra/Websites.java
+++ app/src/main/java/cosapp/com/nostra/Websites.java
@@ -6,7 +6,7 @@ package cosapp.com.nostra;
 
 enum Websites {
     TICKET_MACHINES ("http://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=class_objects&class_id=4000"),
-    GOOGLE_MAPS_REQUEST("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="),
+    GOOGLE_MAPS_REQUEST_LINK("https://maps.googleapis.com/maps/api/distancematrix/json?units=metrics&mode=walking&origins="),
     GOOGLE_MAPS_API_KEY("AIzaSyAn8WHMVkL6gPV45c8M9MRvVwDzXmLtFYI");
 
     private final String website;
