a277d91abd0c4a1c575f273250816c4d3bf9cef5
Kacper Koza <kkoza11@gmail.com>
2016-11-24 21:12:19 +0100

    adding websites with data (json, XML)

--- app/src/main/java/cosapp/com/nostra/Utils.java
+++
@@ -1,9 +0,0 @@
-package cosapp.com.nostra;
-
-/**
- * Created by kkoza on 12.11.2016.
- */
-public class Utils {
-
-
-}
--- app/src/main/java/cosapp/com/nostra/Websites.java
+++ app/src/main/java/cosapp/com/nostra/Websites.java
@@ -5,7 +5,16 @@ package cosapp.com.nostra;
  */
 
 enum Websites {
-    TICKET_MACHINES ("http://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=class_objects&class_id=4000");
+    TICKET_MACHINES ("http://www.poznan.pl/mim/plan/map_service.html?" +
+            "mtype=pub_transport&co=class_objects&class_id=4000"),
+
+    BIKE_STATIONS("https://nextbike.net/maps/nextbike-official.xml"),
+
+    PARKING_MACHINES("http://www.poznan.pl/mim/plan/map_service.html?" +
+            "mtype=pub_transport&co=parking_meters"),
+
+    TICKETS_SALE_POINTS("http://www.poznan.pl/mim/plan/map_service.html" +
+            "?mtype=pub_transport&co=class_objects&class_id=4803");
 
     private final String website;
 
