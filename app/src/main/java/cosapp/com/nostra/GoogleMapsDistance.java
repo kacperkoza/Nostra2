084475aa94a33e62dfee8e80ea3405477dd789f8
Kacper Koza <kkoza11@gmail.com>
2016-11-26 00:44:21 +0100

    class for holding information of address, time and distance of the connection

--- app/src/main/java/cosapp/com/nostra/GoogleMapsDistance.java
+++ app/src/main/java/cosapp/com/nostra/GoogleMapsDistance.java
@@ -4,5 +4,59 @@ package cosapp.com.nostra;
  * Created by kkoza on 25.11.2016.
  */
 
-public class GoogleMapsDistance {
+public class GoogleMapsDistance implements Comparable<GoogleMapsDistance> {
+    private String destinationAddress;
+    private int distanceInMeters;
+    private int walkingTime;
+
+
+    public GoogleMapsDistance(String placeAddress, int distanceInMeters, int walkingTime) {
+        this.destinationAddress = placeAddress;
+        this.distanceInMeters = distanceInMeters;
+        this.walkingTime = walkingTime;
+    }
+
+    @Override
+    public int compareTo(GoogleMapsDistance googleMapsDistance) {
+        if (googleMapsDistance.getWalkingTime() > walkingTime)
+            return -1;
+        else if (googleMapsDistance.getWalkingTime() == walkingTime)
+            return 0;
+        else
+            return 1;
+    }
+
+    @Override
+    public String toString() {
+        return "GoogleMapsDistance{" +
+                "destinationAddress='" + destinationAddress + '\'' +
+                ", distanceInMeters=" + distanceInMeters +
+                ", walkingTime=" + walkingTime +
+                '}';
+    }
+
+    public String getDestinationAddress() {
+        return destinationAddress;
+    }
+
+    public void setDestinationAddress(String destinationAddress) {
+        this.destinationAddress = destinationAddress;
+    }
+
+    public int getDistanceInMeters() {
+        return distanceInMeters;
+    }
+
+    public void setDistanceInMeters(int distanceInMeters) {
+        this.distanceInMeters = distanceInMeters;
+    }
+
+    public int getWalkingTime() {
+        return walkingTime;
+    }
+
+    public void setWalkingTime(int walkingTime) {
+        this.walkingTime = walkingTime;
+    }
+
 }
