f160108ca76262f017a24a44963d9105fd78193a
Kacper Koza <kkoza11@gmail.com>
2016-11-26 00:43:50 +0100

    refactoring and new parser for google maps response

---
+++ app/src/main/java/cosapp/com/nostra/GoogleMapsDistance.java
@@ -0,0 +1,8 @@
+package cosapp.com.nostra;
+
+/**
+ * Created by kkoza on 25.11.2016.
+ */
+
+public class GoogleMapsDistance {
+}
--- app/src/main/java/cosapp/com/nostra/JSON/JSONParser.java
+++ app/src/main/java/cosapp/com/nostra/JSON/JSONParser.java
@@ -1,7 +1,5 @@
 package cosapp.com.nostra.JSON;
 
-import android.util.Log;
-
 import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONArray;
@@ -10,6 +8,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import cosapp.com.nostra.GoogleMapsDistance;
 import cosapp.com.nostra.Place.TicketMachine;
 
 /**
@@ -34,57 +33,81 @@ public class JSONParser {
     /**
      * Parser for JSON text which contains information about:
      * <ul>
-     *     <li>xy coords,</li>
-     *     <li>place name</li>
-     *     <li>place description</li>
-     *     <li>availbility of payment by credit cards</li>
+     * <li>xy coords,</li>
+     * <li>place name</li>
+     * <li>place description</li>
+     * <li>availbility of payment by credit cards</li>
      * </ul>
      *
-     * @param website Text with JSON objects of ticket machines
+     * @param response Text with JSON objects of ticket machines
      * @return ArrayList of the Ticket machines
-     * @see TicketMachine
      * @throws JSONException
+     * @see TicketMachine
      */
-    public static ArrayList<TicketMachine> getTicketMachines(String website) throws JSONException {
-        JSONObject input = new JSONObject(website);
+    public static ArrayList<TicketMachine> getTicketMachines(String response) throws JSONException {
         ArrayList<TicketMachine> machines = new ArrayList<>(70);
+
+        JSONObject input = new JSONObject(response);
         JSONArray features = input.getJSONArray("features");
 
-        for (int i = 0 ; i < features.length() ; i++) {
-
+        for (int i = 0; i < features.length(); i++) {
             TicketMachine ticketMachine = new TicketMachine();
 
             JSONObject object = features.getJSONObject(i);
-            ticketMachine.setID(object.getInt("id"));
-
             JSONObject geometry = object.getJSONObject("geometry");
             JSONArray coordinates = geometry.getJSONArray("coordinates");
-            ticketMachine.setCoordinates(new LatLng(
-                    (Double) coordinates.get(0),
-                    (Double) coordinates.get(1)));
-
             JSONObject properties = object.getJSONObject("properties");
-            ticketMachine.setPlaceName(properties.getString("nazwa"));
 
+            int id = object.getInt("id");
+            LatLng mCoordinates = new LatLng(
+                    (Double) coordinates.get(0),
+                    (Double) coordinates.get(1));
+            String placeName = properties.getString("nazwa");
             String description = properties.getString("opis");
-            ticketMachine.setDescription(deleteHTMLTags(description));
+            description = removeHtmlTags(description);
+            boolean paymentByCreditCard = properties.has("y_4346_karty_p_atnic");
 
-            if (properties.has("y_4346_karty_p_atnic"))
-                ticketMachine.setPaymentByCreditCardAvailable(true);
-            else
-                ticketMachine.setPaymentByCreditCardAvailable(false);
-            Log.d("TAG", ticketMachine.toString());
-
-            machines.add(ticketMachine);
+            machines.add(new TicketMachine(
+                    mCoordinates, placeName, description, paymentByCreditCard, id));
         }
 
         return machines;
     }
 
-    private static String deleteHTMLTags(String stringWithTags) {
-        return stringWithTags.replaceAll("\\<[^>]*>","");
+    private static String removeHtmlTags(String stringWithTags) {
+        return stringWithTags.replaceAll("\\<[^>]*>", "");
     }
 
+    /**
+     * <p>Parser for response from Google Maps API.</p>
+     * <p>Prepared for getting an information about address, walking time and distance between
+     * single origin and list of destinations</p>
+     *
+     * @param response <code>String</code> with response from Google Maps API.
+     * @return <code>ArrayList</code> with <code>GoogleMapsDistance</code> objects which address,
+     * walking time and distance between origin and destination.
+     * @throws JSONException
+     */
+    public static ArrayList<GoogleMapsDistance> parseGoogleMapsResponse(String response) throws JSONException {
+        ArrayList<GoogleMapsDistance> list = new ArrayList<>(100);
 
+        JSONObject jsonObject = new JSONObject(response);
+        JSONArray destinationAddresses = jsonObject.getJSONArray("destination_addresses");
+        JSONArray rows = jsonObject.getJSONArray("rows");
+        JSONArray elements = rows.getJSONArray(0);
 
+        for (int i = 0; i < elements.length(); i++) {
+            JSONObject distance = elements.getJSONObject(i);
+            JSONObject duration = elements.getJSONObject(i);
+            int distanceInMeters = distance.getInt("value");
+            int durationInSeconds = duration.getInt("value");
+            String address = destinationAddresses.getString(i);
+            list.add(new GoogleMapsDistance(address, distanceInMeters, durationInSeconds));
+        }
+        return list;
+    }
 }
+
+
+
+
