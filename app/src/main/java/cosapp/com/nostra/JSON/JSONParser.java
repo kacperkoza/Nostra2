49ee65b3791e7a5a26b99bfec0220fcea5db8e7b
Kacper Koza <kkoza11@gmail.com>
2016-11-26 23:49:09 +0100

    change import, dependencies, launch activity

--- app/src/main/java/cosapp/com/nostra/JSON/JSONParser.java
+++ app/src/main/java/cosapp/com/nostra/JSON/JSONParser.java
@@ -8,7 +8,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-import cosapp.com.nostra.GoogleMapsDistance;
+import cosapp.com.nostra.GoogleMaps.GoogleMapsDistance;
 import cosapp.com.nostra.Place.TicketMachine;
 
 /**
