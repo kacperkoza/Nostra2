1c39d4695b3a22f4a12f6c31066bde3c821e775c
Kacper Koza <kkoza11@gmail.com>
2016-12-07 20:37:36 +0100

    zone changes from char to String

--- app/src/main/java/cosapp/com/nostra/Place/ParkingMachine.java
+++ app/src/main/java/cosapp/com/nostra/Place/ParkingMachine.java
@@ -4,17 +4,25 @@ package cosapp.com.nostra.Place;
  * Created by kkoza on 24.11.2016.
  */
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Class which hold an information about parking machines: coords, zone and street.
  */
 public class ParkingMachine extends Place {
-    private char zone;
+    private String zone;
     private String street;
 
     public ParkingMachine() {
         super(null, "");
-        zone = '0';
+        zone = "0";
         street = "";
+    }
+
+    public ParkingMachine(LatLng coordinates, String placeName, String zone, String street) {
+        super(coordinates, placeName);
+        this.zone = zone;
+        this.street = street;
     }
 
     @Override
@@ -34,11 +42,11 @@ public class ParkingMachine extends Place {
         this.street = street;
     }
 
-    public char getZone() {
+    public String getZone() {
         return zone;
     }
 
-    public void setZone(char zone) {
+    public void setZone(String zone) {
         this.zone = zone;
     }
 }
