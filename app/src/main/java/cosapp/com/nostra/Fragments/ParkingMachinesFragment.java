13ffaa5ec7c15da07f8eb6c48fdb0f9a8465fbeb
Kacper Koza <kkoza11@gmail.com>
2016-12-07 20:36:26 +0100

    Map fragment with parking machines, current position

---
+++ app/src/main/java/cosapp/com/nostra/Fragments/ParkingMachinesFragment.java
@@ -0,0 +1,73 @@
+package cosapp.com.nostra.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+import cosapp.com.nostra.DataManager;
+import cosapp.com.nostra.Place.ParkingMachine;
+import cosapp.com.nostra.R;
+
+public class ParkingMachinesFragment extends Fragment implements OnMapReadyCallback {
+    private DataManager mDataManager;
+    private GoogleMap mMap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_maps, null, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mDataManager = new DataManager(getActivity());
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        ArrayList<ParkingMachine> list = mDataManager.getParkingMachines();
+
+        for (ParkingMachine pm : list) {
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.title(pm.getStreet() + "" + pm.getZone());
+            markerOptions.position(pm.getCoordinates());
+            if (pm.getZone().equals("A")) {
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+            } else if (pm.getZone().equals("B")) {
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+            }
+            mMap.addMarker(markerOptions);
+        }
+        MarkerOptions yourPosition = new MarkerOptions();
+        yourPosition.position(new LatLng(52.405794, 16.930569));
+        yourPosition.title("Twoja pozycja");
+        yourPosition.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+        mMap.addMarker(yourPosition);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52.405794, 16.930569), 12.0f));
+
+
+    }
+}
