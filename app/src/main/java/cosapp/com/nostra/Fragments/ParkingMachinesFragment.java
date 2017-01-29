79936a6618910eeb62a155f13909ade5448a1541
Kacper Koza <kkoza11@gmail.com>
2017-01-29 17:40:43 +0100

    new fragment for displaying ticket shops

--- app/src/main/java/cosapp/com/nostra/Fragments/ParkingMachinesFragment.java
+++ app/src/main/java/cosapp/com/nostra/Fragments/ParkingMachinesFragment.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -20,6 +19,8 @@ import java.util.ArrayList;
 import cosapp.com.nostra.DataManager;
 import cosapp.com.nostra.Place.ParkingMachine;
 import cosapp.com.nostra.R;
+
+import static com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom;
 
 public class ParkingMachinesFragment extends Fragment implements OnMapReadyCallback {
     private DataManager mDataManager;
@@ -49,10 +50,11 @@ public class ParkingMachinesFragment extends Fragment implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         ArrayList<ParkingMachine> list = mDataManager.getParkingMachines();
+        mDataManager.close();
 
         for (ParkingMachine pm : list) {
             MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.title(pm.getStreet() + "" + pm.getZone());
+            markerOptions.title(pm.getStreet() + " " + pm.getZone());
             markerOptions.position(pm.getCoordinates());
             if (pm.getZone().equals("A")) {
                 markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
@@ -61,13 +63,12 @@ public class ParkingMachinesFragment extends Fragment implements OnMapReadyCallb
             }
             mMap.addMarker(markerOptions);
         }
-        MarkerOptions yourPosition = new MarkerOptions();
-        yourPosition.position(new LatLng(52.405794, 16.930569));
-        yourPosition.title("Twoja pozycja");
-        yourPosition.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+
+        MarkerOptions yourPosition = new MarkerOptions()
+                .position(new LatLng(52.405794, 16.930569))
+                .title("Twoja pozycja")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
         mMap.addMarker(yourPosition);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52.405794, 16.930569), 12.0f));
-
-
+        mMap.moveCamera(newLatLngZoom(new LatLng(52.405794, 16.930569), 18.0f));
     }
 }
