d8deed2c7cd7b586f768a61fc11fdfe700f76473
Kacper Koza <kkoza11@gmail.com>
2017-02-05 14:44:16 +0100

    methods for write and read ticket points //refactor plx

--- app/src/main/java/cosapp/com/nostra/DataManager.java
+++ app/src/main/java/cosapp/com/nostra/DataManager.java
@@ -8,10 +8,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import org.joda.time.LocalTime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 import cosapp.com.nostra.Place.ParkingMachine;
 import cosapp.com.nostra.Place.TicketMachine;
+import cosapp.com.nostra.Place.TicketPoint;
 
 /**
  * Created by kkoza on 12.11.2016.
@@ -45,6 +52,23 @@ public class DataManager extends SQLiteOpenHelper {
                         "y REAL," +
                         "street TEXT," +
                         "zone TEXT);");
+
+        sqLiteDatabase.execSQL(
+                "CREATE TABLE ticketPoints(" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "x REAL," +
+                        "y REAL," +
+                        "placeName);");
+
+        sqLiteDatabase.execSQL(
+                "CREATE TABLE ticketPointsHours(" +
+                        "id INTEGER," +
+                        "key INTEGER," +
+                        "openAt TEXT," +
+                        "closeAt TEXT," +
+                        "isOpened24h INTEGER," +
+                        "isClosed24h INTEGER, " +
+                        "FOREIGN KEY(id) REFERENCES ticketPoints(id));");
     }
 
     @Override
@@ -65,6 +89,90 @@ public class DataManager extends SQLiteOpenHelper {
         contentValues.put("placeName", ticketMachine.getPlaceName());
         contentValues.put("description", ticketMachine.getDescription());
         db.insertOrThrow("ticketMachines", null, contentValues);
+    }
+
+    public void addTicketPointToDatabase(TicketPoint ticketPoint) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("x", ticketPoint.getCoordinates().longitude);
+        contentValues.put("y", ticketPoint.getCoordinates().latitude);
+        contentValues.put("placeName",  ticketPoint.getPlaceName());
+        long id = db.insertOrThrow("ticketPoints", null, contentValues);
+
+        ContentValues cValues = new ContentValues();
+        for (int i = 0; i < 3; i++) {
+            HashMap hashmap = ticketPoint.getOpeningHours();
+            cValues.put("id", id);
+            cValues.put("key", i);
+            OpeningHours openingHours = (OpeningHours) hashmap.get(i);
+            if (openingHours != null && openingHours.getClosedAt() != null && openingHours.getOpenedAt() != null) {
+                cValues.put("openAt", localTimeToString(openingHours.getOpenedAt()));
+                cValues.put("closeAt", localTimeToString(openingHours.getClosedAt()));
+            }
+            if (openingHours != null) {
+                int valueToPut = openingHours.isOpenedAllDay() ? 1 : 0;
+                cValues.put("isOpened24h", valueToPut);
+                valueToPut = openingHours.isClosedAllDay() ? 1 : 0;
+                cValues.put("isClosed24h", valueToPut);
+            }
+            db.insertOrThrow("ticketPointsHours", null, cValues);
+        }
+    }
+
+    /**
+     * Convert LocalTime to hh:mm (required for SQLite Datatype)
+     * @param localTime
+     * @return
+     */
+    private String localTimeToString(LocalTime localTime) {
+        return localTime.getHourOfDay() + ":" + localTime.getMinuteOfHour();
+    }
+
+    public ArrayList<TicketPoint> getTicketPoints() throws ParseException {
+        ArrayList<TicketPoint> list = new ArrayList<>(320);
+        Cursor cursor = makeQuery("ticketPoints","id", "x", "y", "placeName");
+        int i = 0;
+        while (cursor.moveToNext()) {
+            SQLiteDatabase db = getReadableDatabase();
+            String[] columns = {"key", "openAt", "closeAt", "isOpened24h", "isClosed24h"};
+            String[] s = {cursor.getString(0)};
+            Cursor c = db.query("ticketPointsHours", columns, "id= ?", s,null, null, null, null);
+           // Cursor c = db.rawQuery("ticketPointsHours", columns, "WHERE id=" + cursor.getInt(i),null, null, null, null);
+            HashMap<Integer, OpeningHours> map = new HashMap<>(3);
+            OpeningHours oh;
+            TicketPoint tp = new TicketPoint();
+            int j = 0;
+            while (c.moveToNext()) {
+                boolean isOpened24h = c.getString(3).equals("1") ? true : false;
+                boolean isClosed24h = c.getString(4).equals("1") ? true : false;
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                Date open = null;
+                Date close = null;
+
+                if (c.getString(1) != null && c.getString(2) != null) {
+                    open = sdf.parse(c.getString(1));
+                    close = sdf.parse(c.getString(2));
+                    oh = new OpeningHours(LocalTime.fromDateFields(open), LocalTime.fromDateFields(close));
+                } else if (isClosed24h == true || isOpened24h == true) {
+                    oh = new OpeningHours();
+                    oh.setOpenedAllDay(isOpened24h);
+                    oh.setClosedAllDay(isClosed24h);
+                } else {
+                    oh = null;
+                }
+
+
+                tp.addOpeningHours(j, oh);
+                j++;
+            }
+            LatLng coords = new LatLng(cursor.getDouble(2), cursor.getDouble(1));
+            tp.setCoordinates(coords);
+            tp.setPlaceName(cursor.getString(3));
+            list.add(tp);
+            i++;
+        }
+        return list;
     }
 
     public void addParkingMachineToTheDatabase(ParkingMachine parkingMachine) {
