14449af5f65c272782ea009df1da68f2d5019742
Kacper Koza <kkoza11@gmail.com>
2017-02-07 01:55:38 +0100

    new method for building simple alert dialog

--- app/src/main/java/cosapp/com/nostra/Utils.java
+++ app/src/main/java/cosapp/com/nostra/Utils.java
@@ -1,6 +1,8 @@
 package cosapp.com.nostra;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 /**
@@ -18,6 +20,19 @@ public final class Utils {
      */
     public static void makeToast(Context context, String text, int length) {
         Toast.makeText(context, text, length).show();
+    }
+
+    public static void makeAlertWindow(Context context, String title, String text) {
+        final AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(text);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 
 }
