607aaad8aa6a89337bb572959230f765a094d59b
Kacper Koza <kkoza11@gmail.com>
2017-02-09 00:28:24 +0100

    No internet connection - load data from database. Otherwise get fresh data

--- app/src/main/java/cosapp/com/nostra/Fragments/BikeStationFragment.java
+++ app/src/main/java/cosapp/com/nostra/Fragments/BikeStationFragment.java
@@ -1,9 +1,10 @@
 package cosapp.com.nostra.Fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,8 @@ import java.util.ArrayList;
 
 import cosapp.com.nostra.CityBikesProvider;
 import cosapp.com.nostra.CurrentLocation;
+import cosapp.com.nostra.DataManager;
+import cosapp.com.nostra.LatLngUtils;
 import cosapp.com.nostra.Place.BikeStation;
 import cosapp.com.nostra.R;
 import cosapp.com.nostra.Utils;
@@ -35,6 +38,7 @@ public class BikeStationFragment extends android.support.v4.app.Fragment impleme
     private GoogleMap mMap;
     private FloatingActionButton fab;
     private ListView lv;
+    private DataManager mDataManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,11 +50,8 @@ public class BikeStationFragment extends android.support.v4.app.Fragment impleme
         mapFragment.getMapAsync(this);
 
         lv = (ListView) view.findViewById(R.id.list);
-
         fab = (FloatingActionButton) view.findViewById(R.id.fab);
-
-        //TODO change calls of GPSTracker methods
-
+        mDataManager = new DataManager(getContext());
 
         return view;
     }
@@ -66,17 +67,30 @@ public class BikeStationFragment extends android.support.v4.app.Fragment impleme
 
         fab.setOnClickListener(new CurrentLocation(getContext(), mMap));
 
-        XMLParser xmlParser = new XMLParser(Websites.BIKE_STATIONS);
+        ArrayList<BikeStation> list;
 
-        String refreshTime = xmlParser.readUpdateTime();
-        Utils.makeToast(getActivity().getApplicationContext(), refreshTime , Toast.LENGTH_LONG);
+        if (Utils.isNetworkAvailable(getContext())) {
+            XMLParser xmlParser = new XMLParser(Websites.BIKE_STATIONS);
 
-        CityBikesProvider cityBikesProvider = xmlParser.readInformationBikesProvider();
-        Log.i("city bikes provider", cityBikesProvider.toString());
+            CityBikesProvider cityBikesProvider = xmlParser.readInformationBikesProvider();
 
-        ArrayList<BikeStation> list = xmlParser.parseNextBike();
+            list = xmlParser.parseNextBike();
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(list.get(0).getCoordinates(), 16.0f));
+            updateDataBase(list);
+
+            String lastUpdateTime = xmlParser.readUpdateTime();
+            updateLastUpdateTimeInSharedPreferences(lastUpdateTime);
+
+            Utils.makeToast(getActivity().getApplicationContext(), lastUpdateTime, Toast.LENGTH_LONG);
+        } else {
+            Utils.makeAlertWindow(getContext(),
+                    getResources().getString(R.string.error),
+                    getResources().getString(R.string.no_access_last_udpate) + " " + getLastUpdateTimeFromSharedPreferences() );
+
+            list = mDataManager.getBikeStations();
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLngUtils.POZNAN, LatLngUtils.NORMAL_ZOOM));
         for (BikeStation bs : list) {
             mMap.addMarker(new MarkerOptions().position(bs.getCoordinates()).title(bs.getPlaceName()));
         }
@@ -87,5 +101,23 @@ public class BikeStationFragment extends android.support.v4.app.Fragment impleme
         names.add("lol");
         lv.setAdapter(names);
 
+    }
+
+    private String getLastUpdateTimeFromSharedPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        return sharedPreferences.getString("lastUpdateTime", "");
+    }
+
+    private void updateLastUpdateTimeInSharedPreferences(String lastUpdateTime) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString("lastUpdateTime", lastUpdateTime);
+        editor.commit();
+
+    }
+
+    private void updateDataBase(ArrayList<BikeStation> list) {
+        for (BikeStation bs : list) {
+            mDataManager.updateBikeStation(bs);
+        }
     }
 }
