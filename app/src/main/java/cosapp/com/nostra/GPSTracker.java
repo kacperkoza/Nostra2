c2af481ef10a6b1f18ce41ee5744af69a9f1164c
Kacper Koza <kkoza11@gmail.com>
2017-02-17 01:34:44 +0100

    simplified implementation with SmartLocation library

--- app/src/main/java/cosapp/com/nostra/GPSTracker.java
+++ app/src/main/java/cosapp/com/nostra/GPSTracker.java
@@ -1,136 +1,36 @@
 package cosapp.com.nostra;
 
 import android.app.AlertDialog;
-import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.os.IBinder;
 import android.provider.Settings;
-import android.util.Log;
 
-import com.google.android.gms.maps.model.LatLng;
-
-import static android.content.ContentValues.TAG;
+import io.nlopez.smartlocation.SmartLocation;
 
 /**
  * Created by kkoza on 03.02.2017.
  */
-public class GPSTracker extends Service implements android.location.LocationListener {
-
-    private static final float MIN_DISTANCE_CHANGE = 10f; // 10 meters
-
-    private static final long MIN_TIME_UPDATE = 1000 * 6; // in ms
-
-    private boolean isGPSEnabled = false;
-
-    private boolean isNetworkEnabled = false;
-
-    private boolean canGetLocation = false;
+public class GPSTracker {
 
     private final Context mContext;
-
-    private LocationManager locationManager;
-
-    private Location location;
 
     public GPSTracker(Context context) {
         this.mContext = context;
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d(TAG, "onLocationChanged: " + location.toString());
+    public boolean isLocationServiceEnabled() {
+        boolean locationServiceEnabled = SmartLocation.
+                with(mContext)
+                .location()
+                .state()
+                .isGpsAvailable();
+
+        return locationServiceEnabled;
     }
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-    }
 
-    @Override
-    public void onProviderEnabled(String s) {
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-    }
-
-    @Override
-    public IBinder onBind(Intent arg0) {
-        return null;
-    }
-
-    public void readCoordinates() {
-        readProviders();
-
-        canGetLocation = isGPSAvailable();
-
-        if (isNetworkEnabled) {
-            readFromNetworkProvider();
-        } else if (isGPSEnabled) {
-            readFromGPSProvider();
-        }
-    }
-
-    private boolean isGPSAvailable() {
-        return isGPSEnabled || isNetworkEnabled;
-    }
-
-    private void readProviders() {
-        locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
-
-        // getting GPS status
-        isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-        // getting network status
-        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-    }
-
-    private void readFromNetworkProvider() throws SecurityException {
-        // First get location from Network Provider
-        if (isNetworkEnabled) {
-            locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER,
-                    MIN_TIME_UPDATE,
-                    MIN_DISTANCE_CHANGE,
-                    this);
-        }
-
-        if (locationManager != null) {
-            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            getCurrentLocation();
-        }
-    }
-
-    private void readFromGPSProvider() throws SecurityException {
-        // if GPS Enabled get lat/long using GPS Services
-        if (isGPSEnabled) {
-            locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    MIN_TIME_UPDATE,
-                    MIN_DISTANCE_CHANGE,
-                    this);
-        }
-
-        if (locationManager != null) {
-            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            getCurrentLocation();
-        }
-    }
-
-    public LatLng getCurrentLocation(){
-            return location != null ? LatLngUtils.locationToLatLng(location) : null;
-    }
-
-    public boolean canGetLocation() {
-        return canGetLocation;
-    }
-
-    public void showSettingsAlert(){
+    public void showLocationSettingsAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
 
         alertDialog.setTitle(mContext.getResources().getString(R.string.cannot_get_gps));
@@ -139,7 +39,7 @@ public class GPSTracker extends Service implements android.location.LocationList
 
         alertDialog.setPositiveButton(mContext.getResources().getString(R.string.settings)
                 , new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int which) {
+            public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 mContext.startActivity(intent);
             }
@@ -153,15 +53,5 @@ public class GPSTracker extends Service implements android.location.LocationList
         });
 
         alertDialog.show();
-    }
-
-    public void stopUsingGPS(){
-        if (locationManager != null){
-            try {
-                locationManager.removeUpdates(GPSTracker.this);
-            } catch (SecurityException e ) {
-                e.printStackTrace();
-            }
-        }
     }
 }
